d6fffe9c45ae8d74ebdcd46d0ca021676fdd2ea6
Ahmed Adel <36866198+AhmedAdelAboEleneen@users.noreply.github.com>
2019-12-19 21:18:14 +0200

    Update PageBase.java

--- src/main/java/page/PageBase.java
+++ src/main/java/page/PageBase.java
@@ -1,17 +1,9 @@
 package page;
 
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
 public class PageBase {
-
-    public WebDriver driver;
-    public JavascriptExecutor jse;
-    public Select select;
-
     // create constructor
     public PageBase(WebDriver driver) {
 
